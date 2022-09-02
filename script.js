28a7e44d62ea9264aef29abcfb0001e6b474343f
João Vitor Lima Lipert <64232339+Lipert1@users.noreply.github.com>
2022-09-02 16:55:15 -0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-var hamburger = document.querySelector(".hamburger");
-
-hamburger.addEventListener("click", function(){
-    document.querySelector(".container").classList.toggle("show-menu");
-});
